60d676530ad11e2e3f9a3eccfc2ee9403aee7912
Alex <alex@bowlinuguru.com>
2016-11-26 20:17:20 +0000

    timeline section

---
+++ sections/logistics.php
@@ -0,0 +1,24 @@
+<section class="section bg-pattern" data-id="logistics" data-name="Logistics">
+     <div class="container-fluid">
+        <div class="row">
+            <h2 class="col-sm-12">
+                Accomodations 
+            </h2>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row table">
+            <div class="col-sm-7 cell">
+                
+            </div>
+            <div class="col-md-5 cell">
+                <div class="story">
+                  <p>
+                    Brr
+                  </p>
+                </div>
+            </div>
+        </div>
+    </div>
+        <!-- / map-wrap -->
+</section>
---
+++ sections/timeline.php
@@ -0,0 +1,143 @@
+<section class="section bg-pattern" data-id="timeline" data-name="Timeline">
+     <div class="container-fluid">
+        <div class="row">
+            <h2 class="col-sm-12">
+                Timeline
+            </h2>
+        </div>
+        
+        <div class="timeline">
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="day">
+                            July 21st, 2017
+                        </span>        
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="time">
+                            5:00 pm
+                        </span>
+                        <h3>Prenuptial Dinner</h3>
+                        <p class="description">
+                            Please plan on joining us for dinner and games as our family
+                            and friends get to know each other.  Check back later for 
+                            more details.
+                        </p>                
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="day">
+                            July 22nd, 2017
+                        </span>        
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="time">
+                            4:15 pm
+                        </span>
+                        <h3>Arrive at Arapahoe Basin</h3>
+                        <p class="description">
+                            Please plan on arriving by 4:15 pm at the latest. The 6th Alley
+                            Bar at the base of Arapahoe Basin will be open.
+                        </p>                
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="time">
+                            4:30 pm
+                        </span>
+                        <h3>Chairlift begins Running</h3>
+                        <p class="description">
+                            At 4:30, the chair lift to the venue will start running
+                        </p>                
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="time">
+                             5:00 pm
+                        </span>
+                        <h3>Ceremony</h3>
+                        <p class="description">
+                            OMG, its happening!
+                        </p>                
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="time">
+                             5:30 pm
+                        </span>
+                        <h3>Cocktail Hour</h3>
+                        <p class="description">
+                            Enjoy some hors d'oeuvres and drinks while we take pictures.
+                        </p>                
+                    </div>
+                </div>
+            </div>
+            <div class="row table">
+                <div class="cell">
+                    <div class="bar">
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="event">
+                        <span class="time">
+                             6:30 pm
+                        </span>
+                        <h3>Dinner</h3>
+                        <p class="description">
+                            Enjoy some hors d'oeuvres and drinks while we take pictures.
+                        </p>                
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
