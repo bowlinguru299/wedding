cc077daf8eaf2920a3e18e8c7748b82267f89788
Alex <alex@bowlinuguru.com>
2017-04-22 22:24:09 +0000

    Add registry

---
+++ sections/registry.php
@@ -0,0 +1,21 @@
+<section class="section highlight-section" data-id="registry" data-name="Registry">
+     <div class="container-fluid">
+        <div class="row">
+            <h2 class="col-sm-12">
+                Registry
+            </h2>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row table">
+            <a class="cell registry-item" href="https://www.zola.com/registry/twobecomeweino">
+                <img src="https://www.zola.com/assets/images/registryShare/zola-logo-black.jpg" />
+            </a>
+            <div class="cell registry-item-spacer">
+            </div>
+            <a class="cell registry-item" href="https://www.crateandbarrel.com/gift-registry/lyndsay-taylor-and-alex-weingarten/r5600202">
+                <img src="http://aisleperfect.com/wp-content/uploads/2017/04/Crate-and-Barrel-Logo-600x400.jpeg" />
+            </a>
+        </div>
+    </div>
+</section>
--- sections/rsvp.php
+++ sections/rsvp.php
@@ -8,10 +8,10 @@
         </div>
         
         <div class="iframe-wrapper">
-        <iframe scrolling="no" src="https://docs.google.com/forms/d/e/1FAIpQLSe9yB2sMlRaz7oX29GgsdzBLSoaWo2xyUEI_uV01S6p92psuw/viewform?embedded=true" 
-             frameborder="0" marginheight="0" marginwidth="0">
-            Loading...
-        </iframe>
+            <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSe9yB2sMlRaz7oX29GgsdzBLSoaWo2xyUEI_uV01S6p92psuw/viewform?embedded=true" 
+                 frameborder="0" marginheight="0" marginwidth="0">
+                Loading...
+            </iframe>
         </div>
     </div>
 </section>
